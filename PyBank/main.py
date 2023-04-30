9dd0bd66ede0e1ed2a3bf05cab398ac0e35dd8d7
Wook22 <1994wook@gmail.com>
2023-04-30 16:30:11 -0700

    firstcommit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,70 +1,60 @@
 import os
 import csv
 
+# Set the file path for the budget data
 budget_data = os.path.join("..", "Resources", "budget_data.csv")
 
-# Lists to store data
+# Create empty lists to store data
 year = []
 day = []
 date = []
 profit = []
-
 increase = []
 inc_date = []
 inc_index = []
-
 decrease = []
 dec_date = []
 dec_index = []
 
-
+# Initialize variables for calculations
 total_month = 0
 total_amount = 0
-value = 0
 average_change = 0
 
-
-# with open(udemy_csv, encoding='utf-8') as csvfile:
+# Open the CSV file and read the data using the csv module
 with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    # Skip the header row
     csvheader = next(csvreader)
     
-    
+    # Loop through each row in the CSV file
     for row in csvreader:
-        # Add Date
+        # Extract the date, year, and day from the current row
         date.append(row[0])
-        
-        # Split the date into year and day
         split_date = row[0].split("-")
         year.append(split_date[0])
         day.append(split_date[1])
         
-
-        # Add Profit
+        # Extract the profit/loss value from the current row
         profit.append(row[1])
         
-        # Add count total months
+        # Increment the total month count and total amount variables
         total_month = total_month + 1
-        
-        # Add all total amount
         total_amount = total_amount + int(row[1])
     
-    #Average 
-    average_change = sum(profit)/len(profit)
+    # Calculate the average change in profit/loss over the entire period
+    average_change = sum(profit) / len(profit)
         
-    #Greatest Increase
+    # Find the greatest increase and decrease in profit/loss over the entire period
     increase = max(profit)
     inc_index = profit.index(increase)
     inc_date = date[inc_index]
-
-    #Greatest Decrease
     decrease = min(profit)
     dec_index = profit.index(decrease)
     dec_date = date[dec_index]
     
-
-
-#Displaying information
+# Print out the results to the console
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {str(total_month)}")
@@ -72,9 +62,9 @@ print(f"Total: ${str(total_amount)}")
 print(f"Average Change: ${str(round(average_change,2))}")
 print(f"Greatest Increase in Profits: {inc_date} (${str(increase)})")
 print(f"Greatest Decrease in Profits: {dec_date} (${str(decrease)})")
-   
 
-with open("result.txt", "w") as txtfile:
+# Write the results to a text file
+with open('result.txt', 'x') as txtfile:
     txtfile.write("Financial Analysis")
     txtfile.write("\n")
     txtfile.write("----------------------------")
@@ -88,4 +78,3 @@ with open("result.txt", "w") as txtfile:
     txtfile.write(f"Greatest Increase in Profits: {inc_date} (${str(increase)})")
     txtfile.write("\n")
     txtfile.write(f"Greatest Decrease in Profits: {dec_date} (${str(decrease)})")
-    
