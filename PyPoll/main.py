5ecc5d472d1415a5261b5b0bbb0080bda8731730
Wook22 <1994wook@gmail.com>
2023-04-30 18:26:14 -0700

    end result

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,43 +1,63 @@
 import os
 import csv
 
-election_data = os.path.join("..", "Resources", "election_data.csv")
+# Set the file path for the election data
+election_data = os.path.join("Resources", "election_data.csv")
 
-# Lists to store data
+# Initialize empty lists to store data
 ID = []
 county = []
 candidate = []
-
 candidate_vote = []
-candidate_percantage = []
-
+candidate_percentage = []
+Winner = []
 total_vote = 0
 
-
-
-# with open(udemy_csv, encoding='utf-8') as csvfile:
+# Open the CSV file
 with open(election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csvheader = next(csvreader)
     
+    # Loop through each row in the CSV file
     for row in csvreader:
-        # The total number of votes cast
+        # Count the total number of votes cast
         total_vote = total_vote + 1
         
-        # A complete list of candidates who received votes
+        # Add the candidate's name and vote to the candidate and candidate_vote lists
         if row[2] not in candidate:
             candidate.append(row[2])
-            
-            
-        
-        # The percentage of votes each candidate won
-        
-        
-        # The total number of votes each candidate won
-        
-        
-        # The winner of the election based on popular vote.
+            candidate_vote.append(1)
+        else:
+            candidate_vote[candidate.index(row[2])] = candidate_vote[candidate.index(row[2])] + 1        
+    
+    # Calculate the percentage of votes each candidate won
+    for i in candidate_vote:
+        candidate_percentage.append(round((i/total_vote)*100,3))
+    
+    # Find the winner of the election based on popular vote
+    Winner = candidate[candidate_vote.index(max(candidate_vote))]
 
+# Print out the results to the console
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {str(total_vote)}")
+for j in range(len(candidate)):
+    print(f"{candidate[j]}: {str(candidate_percentage[j])}% ({str(candidate_vote[j])})")
+print("-------------------------")
+print(f"Winner: {Winner}")
+print("-------------------------")
 
-
-        
+# Write the results to a text file
+with open('result.txt', 'x') as txtfile:
+    txtfile.write("Election Results")
+    txtfile.write("\n")
+    txtfile.write("-------------------------")
+    txtfile.write("\n")
+    for k in range(len(candidate)):
+        txtfile.write(f"{candidate[k]}: {str(candidate_percentage[k])}% ({str(candidate_vote[k])})")
+        txtfile.write("\n")
+    txtfile.write("-------------------------")
+    txtfile.write("\n")
+    txtfile.write(f"Winner: {Winner}")
+    txtfile.write("\n")
+    txtfile.write("-------------------------")
